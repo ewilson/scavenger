55cbd45b6a18e6d4cb9fac82671cd8097735e710
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-06 22:55:31 -0400

    sending data --in progress

--- go.py
+++ go.py
@@ -1,13 +1,23 @@
 import os
 
+import requests
+import json
+
 from questions import Question
 from hashanswer import DELIM
+import update
+
+
+def take_name():
+    name = raw_input("Please enter your name: \n")
+    return update.send_name(name)
 
 def display_status():
     os.system('cls') if os.name == 'nt' else os.system('clear')
     status_indicator = ['  ' if n in questions else '##' for n in range(TOTAL_Q)]
     print '|%s|' % '|'.join(status_indicator)
     print '%d of %d ANSWERS FOUND\n' % (TOTAL_Q - len(questions), TOTAL_Q)
+
 
 def list_unsolved_questions():
     question_limit = TOTAL_Q - len(questions) + 2
@@ -16,7 +26,8 @@ def list_unsolved_questions():
             break
         print '%d) %s' % (k, questions[k].question)
 
-def take_guess():
+
+def take_guess(pid):
     q_raw = raw_input('\nWhich question? ')
     if q_raw.isdigit() and int(q_raw) in questions:
         q = int(q_raw)
@@ -31,12 +42,15 @@ def take_guess():
         print 'That number choice is invalid'
     raw_input('Press [Enter] to continue\n')
 
+
 def go():
+    pid = take_name()
     while questions:
         display_status()
         list_unsolved_questions()
-        take_guess()
+        take_guess(pid)
     print 'Congratulations! You have completed the scavenger hunt!'
+
 
 def load_questions(filename):
     with open(filename) as f:
